e3ea91be5042c6b141b40e491eb1dec4a463e485
Sandeep Sharma <sandeeptech8@gmail.com>
2019-09-21 15:49:13 +0530

    Create Node.java
    supporting Node class/structure.

---
+++ BinaryTree/Node.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+public class Node {
+    
+    int data;
+    Node leftChild;
+    Node rightChild;
+    
+    // constructor
+    Node(int data){
+        this.data = data;
+    }
+    
+}
